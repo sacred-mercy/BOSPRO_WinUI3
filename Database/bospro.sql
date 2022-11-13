2078db41c903131c147e930d870a1235e8bf5199
gauravnegi9634@gmail.com <gauravnegi9634@gmail.com>
2022-11-13 19:15:14 +0530

    Added Database

---
+++ Database/bospro.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2022 at 02:43 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bospro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `programs`
+--
+
+CREATE TABLE `programs` (
+  `program_code` varchar(10) NOT NULL,
+  `program_name` tinytext NOT NULL,
+  `program_college` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `programs`
+--
+
+INSERT INTO `programs` (`program_code`, `program_name`, `program_college`) VALUES
+('BSCIT', 'Bachelors of Informatin Technology', 'USCS'),
+('BCA', 'bachelors of COmputer application', 'UIT');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `email` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` char(100) NOT NULL,
+  `role` text NOT NULL,
+  `college` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`email`, `password`, `name`, `role`, `college`) VALUES
+('g', 'g', 'Gaurav Singh', 'user', 'USCS'),
+('a', 'a', 'Admin Singh', 'admin', 'UIT'),
+('f', 'f', 'Faurav Singh', 'user', 'UIM');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
