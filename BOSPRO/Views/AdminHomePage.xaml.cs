645c58f587deb5767bde941e67899041723239ad
gauravnegi9634@gmail.com <gauravnegi9634@gmail.com>
2022-11-19 21:46:16 +0530

    Program Remove Complete

--- BOSPRO/Views/AdminHomePage.xaml.cs
+++ BOSPRO/Views/AdminHomePage.xaml.cs
@@ -15,7 +15,16 @@ public sealed partial class AdminHomePage : Page
     {
         ViewModel = App.GetService<AdminHomeViewModel>();
         InitializeComponent();
-        ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+        // To fetch Admin name
+        _ = Windows.Storage.ApplicationData.Current.LocalSettings;
         //AdminName.Content = localSettings.Values["name"] as string;
+
+        // TODO: Add Programs Name from DB to ProgramRemoveComboBox for reference see UserHomePage.xaml.cs
+    }
+
+    private void programRemovebtn_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        // TODO: Delete selected program from Database
     }
 }
